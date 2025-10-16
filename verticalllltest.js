1257be0da0263faad907f5b7e7b251c8dfbad100
amukherjee93 <a.o.mukherjee@accenture.com>
2025-10-16 22:56:01 +0100

    Add files via upload

---
+++ verticalllltest.js
@@ -0,0 +1,38 @@
+class VerticalText extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.render();
+  }
+
+  static get observedAttributes() {
+    return ['text'];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === 'text') {
+      this.render();
+    }
+  }
+
+  render() {
+    const text = this.getAttribute('text') || 'VERTICALLLL';
+
+    this.shadowRoot.innerHTML = `
+      <style>
+        .rotated {
+          transform: rotate(-90deg);
+          transform-origin: left top;
+          font-family: Arial, sans-serif;
+          font-size: 24px;
+          color: #333;
+          white-space: nowrap;
+          display: inline-block;
+        }
+      </style>
+      <div class="rotated">${text}</div>
+    `;
+  }
+}
+
+customElements.define('vertical-text', VerticalText);
