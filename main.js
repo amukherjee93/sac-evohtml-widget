2fbfe7f8d79febd3584bf2b026008693c08fcebc
amukherjee93 <a.o.mukherjee@accenture.com>
2025-10-16 22:05:35 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,27 +1,8 @@
 class VerticalText extends HTMLElement {
   constructor() {
     super();
-    const shadow = this.attachShadow({ mode: 'open' });
-
-    const wrapper = document.createElement('div');
-    wrapper.setAttribute('class', 'vertical-text');
-
-    const text = this.getAttribute('text') || 'VERTICAL';
-    wrapper.innerHTML = text.split('').join('<br>'); // Breaks each letter into a new line
-
-    const style = document.createElement('style');
-    style.textContent = `
-      .vertical-text {
-        font-family: Arial, sans-serif;
-        font-size: 16px;
-        white-space: pre;
-        line-height: 1.2;
-        text-align: center;
-      }
-    `;
-
-    shadow.appendChild(style);
-    shadow.appendChild(wrapper);
+    this.attachShadow({ mode: 'open' });
+    this.render();
   }
 
   static get observedAttributes() {
@@ -30,8 +11,26 @@ class VerticalText extends HTMLElement {
 
   attributeChangedCallback(name, oldValue, newValue) {
     if (name === 'text') {
-      this.shadowRoot.querySelector('.vertical-text').innerHTML = newValue.split('').join('<br>');
+      this.render();
     }
+  }
+
+  render() {
+    const text = this.getAttribute('text') || 'VERTICAL';
+    const formattedText = text.split('').join('<br>');
+    this.shadowRoot.innerHTML = `
+      <style>
+        .vertical-text {
+          font-family: Arial, sans-serif;
+          font-size: 16px;
+          color: #333;
+          line-height: 1.2;
+          text-align: center;
+          white-space: pre;
+        }
+      </style>
+      <div class="vertical-text">${formattedText}</div>
+    `;
   }
 }
 
